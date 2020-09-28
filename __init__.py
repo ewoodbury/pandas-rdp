8652f774c25887755b37b8352ebd2e62fa80ad17
ewoodbury <woodbury.e@berkeley.edu>
2020-09-27 23:08:24 -0700

    Add full rdp algorithm

--- __init__.py
+++ __init__.py
@@ -12,18 +12,32 @@ def get_distance(start, end, p):
     return d
 
 
-def rdp(df, columns, epsilon=0):
+def rdp(df, xcol, ycol, epsilon=0.001):
+    col_0 = df.columns.get_loc(xcol)
+    col_1 = df.columns.get_loc(ycol)
+    np_data = df.to_numpy()
     index = 0
     d_max = 0
-    col_0 = df.columns.get_loc(columns[0])
-    col_1 = df.columns.get_loc(columns[1])
 
-    start = np.array([df.to_numpy()[0, col_0], df.to_numpy()[0, col_1]])
-    end = np.array([df.to_numpy()[0, col_0], df.to_numpy()[0, col_1]])
+    start = np.array([np_data[0, col_0], np_data[0, col_1]])
+    end = np.array([np_data[0, col_0], np_data[0, col_1]])
 
-    for i, row in enumerate(df.to_numpy()):
+    for i, row in enumerate(np_data):
         d = get_distance(start, end, np.array([row[col_0], row[col_1]]))
         if d > d_max:
             index = i
             d_max = d
 
+    if d_max > epsilon:
+        result_0 = rdp(
+            pd.DataFrame(np_data[: index + 1], columns=df.columns), xcol, ycol, epsilon
+        )
+        result_1 = rdp(
+            pd.DataFrame(np_data[index:], columns=df.columns), xcol, ycol, epsilon
+        )
+        result = pd.concat(
+            [result_0, result_1]
+        )  # result_0 and result_1 are dataframes.
+    else:
+        result = df
+    return result
