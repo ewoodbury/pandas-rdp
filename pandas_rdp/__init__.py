515f0d8666b64bc61d666069eafe05f7274e6588
ewoodbury <woodbury.e@berkeley.edu>
2020-09-29 23:10:11 -0700

    Improve performance with native python lists

--- pandas_rdp/__init__.py
+++ pandas_rdp/__init__.py
@@ -18,11 +18,11 @@ def get_distance(start, end, p):
 def rdp_reduce(np_data, xcol_ind, ycol_ind, epsilon=0.001):
     index = 0
     d_max = 0
-    start = np.array([np_data[0, xcol_ind], np_data[0, ycol_ind]])
-    end = np.array([np_data[-1, xcol_ind], np_data[-1, ycol_ind]])
+    start = [np_data[0, xcol_ind], np_data[0, ycol_ind]]
+    end = [np_data[-1, xcol_ind], np_data[-1, ycol_ind]]
 
     for i, row in enumerate(np_data):
-        d = get_distance(start, end, np.array([row[xcol_ind], row[ycol_ind]]))
+        d = get_distance(start, end, [row[xcol_ind], row[ycol_ind]])
         if d > d_max:
             index = i
             d_max = d
