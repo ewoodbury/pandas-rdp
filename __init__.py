3e7c9159190a181761761a98f57b369d3c1c3435
ewoodbury <woodbury.e@berkeley.edu>
2020-09-29 22:59:57 -0700

    Rename column index variables

--- __init__.py
+++ __init__.py
@@ -15,21 +15,21 @@ def get_distance(start, end, p):
     ) / sqrt(y_diff ** 2 + x_diff ** 2)
 
 
-def rdp_reduce(np_data, col_0, col_1, epsilon=0.001):
+def rdp_reduce(np_data, xcol_ind, ycol_ind, epsilon=0.001):
     index = 0
     d_max = 0
-    start = np.array([np_data[0, col_0], np_data[0, col_1]])
-    end = np.array([np_data[-1, col_0], np_data[-1, col_1]])
+    start = np.array([np_data[0, xcol_ind], np_data[0, ycol_ind]])
+    end = np.array([np_data[-1, xcol_ind], np_data[-1, ycol_ind]])
 
     for i, row in enumerate(np_data):
-        d = get_distance(start, end, np.array([row[col_0], row[col_1]]))
+        d = get_distance(start, end, np.array([row[xcol_ind], row[ycol_ind]]))
         if d > d_max:
             index = i
             d_max = d
 
     if d_max > epsilon:
-        result_0 = rdp_reduce(np_data[: index + 1], col_0, col_1, epsilon)
-        result_1 = rdp_reduce(np_data[index:], col_0, col_1, epsilon)
+        result_0 = rdp_reduce(np_data[: index + 1], xcol_ind, ycol_ind, epsilon)
+        result_1 = rdp_reduce(np_data[index:], xcol_ind, ycol_ind, epsilon)
         result = np.concatenate([result_0, result_1])
     else:
         result = np.concatenate([[np_data[0]], [np_data[-1]]])
@@ -38,10 +38,10 @@ def rdp_reduce(np_data, col_0, col_1, epsilon=0.001):
 
 def rdp(df, xcol, ycol, epsilon=0.001):
     if isinstance(df, pd.DataFrame):
-        col_0 = df.columns.get_loc(xcol)
-        col_1 = df.columns.get_loc(ycol)
+        xcol_ind = df.columns.get_loc(xcol)
+        ycol_ind = df.columns.get_loc(ycol)
         np_data = df.to_numpy()
-        result = rdp_reduce(np_data, col_0, col_1, epsilon=0.001)
+        result = rdp_reduce(np_data, xcol_ind, ycol_ind, epsilon=0.001)
         return pd.DataFrame(result, columns=df.columns).drop_duplicates(
             ignore_index=True
         )
